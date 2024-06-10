216cdc7dc963fe39699d9af07d2ba742e19f87da
luizfelipezorzette <67935567+luizfelipezorzette@users.noreply.github.com>
2024-06-10 19:59:09 -0300

    Cliente.java

--- Jokenpo-GIT/Cliente.java
+++ Jokenpo-GIT/Cliente.java
@@ -1,59 +1,144 @@
 package jokenpo;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
+
 public class Cliente {
     public static void main(String[] args) {
-        // Define o endereço do servidor e a porta a ser usada
-        final String ENDERECO_SERVIDOR = "localhost"; // "localhost" refere-se ao endereço do próprio computador
-        final int PORTA = 12345;
+        
+         String ENDERECO_SERVIDOR = "localhost"; // "localhost" refere-se ao endereço do próprio computador
+         int PORTA = 12345;
+         String clienteIP;
+         String clientePorta;
 
         try {
-            // Conecta-se ao servidor utilizando o endereço e a porta especificados
-            Socket socket = new Socket(ENDERECO_SERVIDOR, PORTA);
-            // Imprime uma mensagem indicando que a conexão com o servidor foi estabelecida com sucesso
-            System.out.println("Conectado ao servidor.");
-
-            // Configura um scanner para receber dados do servidor através do socket
-            Scanner entrada = new Scanner(socket.getInputStream());
-            // Configura um PrintWriter para enviar dados para o servidor através do socket
-            PrintWriter saida = new PrintWriter(socket.getOutputStream(), true);
-            // Configura um scanner para ler entrada do teclado do usuário
             Scanner teclado = new Scanner(System.in);
 
-            // Declara variáveis para armazenar mensagens do servidor e do cliente
+            System.out.printf("Informe o IP do servidor(default: %s): ", ENDERECO_SERVIDOR);
+            clienteIP = teclado.nextLine();
+            if (ENDERECO_SERVIDOR.length() > 0) {
+                ENDERECO_SERVIDOR = clienteIP;
+            }
+            
+            System.out.printf("Informe a porta do servidor(default: %d): ", PORTA);
+            clientePorta = teclado.nextLine();
+            if (clientePorta.length() > 0) {
+                PORTA = Integer.parseInt(clientePorta);
+            }
+            Socket socket = new Socket(ENDERECO_SERVIDOR, PORTA);
+            
+            System.out.println("Conectado ao servidor.");
+
+            
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter saida = new PrintWriter(socket.getOutputStream(), true);
+           
+            
+            String modo;
             String mensagemDoServidor;
             String mensagemDoCliente;
 
-            // Loop para receber mensagens do servidor continuamente
-            while (entrada.hasNextLine()) {
-                // Lê uma linha da mensagem enviada pelo servidor
-                mensagemDoServidor = entrada.nextLine();
-                // Imprime a mensagem recebida do servidor no console do cliente
-                System.out.println("Servidor: " + mensagemDoServidor);
 
-                // Verifica se a mensagem recebida indica uma mensagem especial do servidor
+
+
+
+
+            System.out.println("Bem vindo! ao Pedra,Papel e Tesoura\n"+
+                                      "Escolha o modo de jogo\n"+
+                                      "Singleplayer (S)\n"+
+                                      "Multiplayer (M)" );
+
+                modo = teclado.nextLine();
+                saida.println(modo);
+            if(modo.equalsIgnoreCase("S")){
+               while (true) {
+                
+                System.out.println("Escolha entre Pedra,Papel ou Tesoura!");
+                mensagemDoCliente = teclado.nextLine();
+                
+                saida.println(mensagemDoCliente);
+                
+
+                
+                mensagemDoServidor = entrada.readLine();
+                
+                System.out.println("Servidor: " + mensagemDoServidor);
+                mensagemDoServidor = entrada.readLine();
+                System.out.println("Servidor: " + mensagemDoServidor);
+                mensagemDoServidor = entrada.readLine();
+                System.out.println("Servidor: " + mensagemDoServidor);
+                mensagemDoServidor = entrada.readLine();
+               
+                System.out.println("Servidor: " + mensagemDoServidor);
+                mensagemDoCliente = teclado.nextLine();
+                saida.println(mensagemDoCliente);
+
+                
                 if (mensagemDoServidor.equals("Bem-vindo ao jogo Pedra, Papel e Tesoura!") ||
                     mensagemDoServidor.equals("Você venceu!") ||
                     mensagemDoServidor.equals("Você perdeu!") ||
                     mensagemDoServidor.equals("Empate!")) {
-                    // Se for uma mensagem especial, continua esperando por mais mensagens do servidor
+                   
                     continue;
                 }
 
-                // Verifica se a mensagem recebida indica que o servidor encerrou a conexão
-                if (mensagemDoServidor.equals("sair")) {
-                    // Se o servidor indicar que o cliente deve sair, o loop é encerrado
+                
+                else if (mensagemDoServidor.equals("sair")) {
+                    
                     break;
                 }
 
-                // Solicita entrada do usuário para enviar ao servidor
-                mensagemDoCliente = teclado.nextLine();
-                // Envia a mensagem do cliente para o servidor através do socket
-                saida.println(mensagemDoCliente);
+               }
+
+
+
+            }else if(modo.equalsIgnoreCase("m")){
+
+                while (true) {
+                    
+                
+                    System.out.println("Escolha entre Pedra,Papel ou Tesoura!");
+                    mensagemDoCliente = teclado.nextLine();
+                    
+                    saida.println(mensagemDoCliente);
+                    
+                    mensagemDoServidor = entrada.readLine();
+                    
+                    System.out.println("Servidor: " + mensagemDoServidor);
+                    mensagemDoServidor = entrada.readLine();
+                    System.out.println("Servidor: " + mensagemDoServidor);
+                    mensagemDoServidor = entrada.readLine();
+                     System.out.println("Servidor: " + mensagemDoServidor);
+                    mensagemDoServidor = entrada.readLine();
+                   
+                    System.out.println("Servidor: " + mensagemDoServidor);
+                    mensagemDoCliente = teclado.nextLine();
+                    saida.println(mensagemDoCliente);
+    
+                    
+                    if (mensagemDoServidor.equals("Bem-vindo ao jogo Pedra, Papel e Tesoura!") ||
+                        mensagemDoServidor.equals("Você venceu!") ||
+                        mensagemDoServidor.equals("Você perdeu!") ||
+                        mensagemDoServidor.equals("Empate!")) {
+                       
+                        continue;
+                    }
+    
+                    
+                    else if (mensagemDoServidor.equals("sair")) {
+                        
+                        break;
+                    }
+    
+                   }
+
+                
             }
+            
 
             // Fecha os scanners e o socket após a conclusão da interação
             entrada.close();
